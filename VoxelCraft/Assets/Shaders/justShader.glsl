9eecac3ea52a0f98bb2b35a7e5e7d411990dc6af
Vergil <Vergil@DESKTOP-NESDP5L>
2024-03-04 16:21:49 +0300

    Updated shader asset

--- VoxelCraft/Assets/Shaders/justShader.glsl
+++ VoxelCraft/Assets/Shaders/justShader.glsl
@@ -1,3 +1,18 @@
+#type fragment
+
+#version 460 core
+
+in vec3 v_Position;
+in vec4 v_Color;
+
+void main()
+{
+	gl_FragColor = vec4(v_Position * 0.5 + 0.5, 1.0);
+	gl_FragColor = v_Color;
+}
+
+#type vertex
+
 #version 460 core
 			
 layout(location = 0) in vec3 a_Position;
