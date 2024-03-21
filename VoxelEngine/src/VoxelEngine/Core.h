61801e650db3de8cf2b9430d94aeeb2794b80a3e
Denis <rexarx@bk.ru>
2024-03-21 21:17:08 +0300

    CMake changed build path

--- VoxelEngine/src/VoxelEngine/Core.h
+++ VoxelEngine/src/VoxelEngine/Core.h
@@ -24,4 +24,4 @@
 
 #define VE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
 
-#define ROOT std::filesystem::current_path().parent_path().parent_path().parent_path().string()
+#define ROOT std::filesystem::current_path().parent_path().parent_path().string()
