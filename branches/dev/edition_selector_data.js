050d05eceba888e842bab58a2a36eb520ac0d1de
RexarX <RexarX@users.noreply.github.com>
2026-07-04 23:38:01 +0000

    deploy: 149c55b54c353a3a48cbc4608802126e380c71e9

---
+++ branches/dev/edition_selector_data.js
@@ -0,0 +1,3 @@
+window.__HELIOS_DEFAULT_VERSION__ = "0.1.0";
+window.__HELIOS_BRANCH_SELECTOR_HTML__ = "<select id=\"branchSelector\" title=\"Documentation branch\">\n  <option value=\"main\">main</option>\n  <option value=\"branches/dev\" selected>dev</option>\n</select>";
+window.__HELIOS_VERSION_SELECTOR_HTML__ = "<select id=\"versionSelector\" title=\"Documentation version\">\n  <option value=\"0.1.0\" selected>0.1.0</option>\n</select>";
