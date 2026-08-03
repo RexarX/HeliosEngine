357bb48b4c3b1a34f80d4889ab4886e047209512
RexarX <who727cares@gmail.com>
2026-08-04 00:37:27 +0300

    Removed debugging from tests and macOS workflow

--- src/log/tests/main.cpp
+++ src/log/tests/main.cpp
@@ -1,25 +1,2 @@
-#define DOCTEST_CONFIG_IMPLEMENT
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
-
-#include <helios/log/logger.hpp>
-
-#include <cstdio>
-
-int main(int argc, char* argv[]) {
-  int result = 0;
-  {
-    doctest::Context context;
-    context.applyCommandLine(argc, argv);
-    result = context.run();
-  }
-
-  std::fputs("[helios_log_tests] after doctest\n", stderr);
-  std::fflush(stderr);
-
-  helios::log::Logger::Instance().Shutdown();
-
-  std::fputs("[helios_log_tests] after Logger::Shutdown\n", stderr);
-  std::fflush(stderr);
-
-  return result;
-}
