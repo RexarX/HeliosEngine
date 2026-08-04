910c5fb6d57fc4a3b81e6e69b94cfdfddecc5650
RexarX <RexarX@users.noreply.github.com>
2026-08-04 20:19:22 +0000

    deploy: be64ca38a54a9ab9be6402ae8ce86651f8155efb

--- branches/dev/search/pages_7.js
+++ branches/dev/search/pages_7.js
@@ -5,7 +5,7 @@ var searchData=
   ['deferred_20mutations_2',['Commands — Deferred Mutations',['../dir_ddca5f149c4ce13074f28b270e950ba9.html#commands--deferred-mutations',1,'']]],
   ['definition_3',['Module Definition',['../dir_217ee790cdc210ca56feb9a8b7616066.html#module-definition',1,'']]],
   ['definition_20cmakelists_20txt_4',['Build definition (&lt;span class=&quot;tt&quot;&gt;CMakeLists.txt&lt;/span&gt;)',['../md_docs_2guidelines.html#build-definition-cmakeliststxt',1,'']]],
-  ['delegate_5',['Delegate',['../dir_313caf1132e152dd9b58bea13a4052ca.html#delegate',1,'']]],
+  ['delegate_5',['Delegate',['../dir_aebb8dcc11953d78e620bbef0b9e2183.html#delegate',1,'']]],
   ['dependencies_6',['Dependencies',['../dir_5194e5fea318fda12687127c23f8aba1.html#dependencies',1,'Dependencies'],['../dir_b60a79ed93f318a37dbc9781b4452c17.html#dependencies-1',1,'Dependencies'],['../dir_1b313d86a3cedf427c8a82c0995b1bb6.html#dependencies-2',1,'Dependencies'],['../dir_aebb8dcc11953d78e620bbef0b9e2183.html#dependencies-3',1,'Dependencies'],['../dir_ddca5f149c4ce13074f28b270e950ba9.html#dependencies-4',1,'Dependencies'],['../dir_2241064965c8d9ca993f08b926076f7d.html#dependencies-5',1,'Dependencies'],['../dir_94d7edf6f8f3d9af5fd63c44172da41c.html#dependencies-6',1,'Dependencies'],['../dir_2730ace12cf03b0ba989133b5ac743ea.html#dependencies-7',1,'Dependencies'],['../dir_313caf1132e152dd9b58bea13a4052ca.html#dependencies-8',1,'Dependencies'],['../dir_c7f79d9fead0a70cab4af348b59fbdf1.html#dependencies-9',1,'Dependencies'],['../index.html#installing-dependencies',1,'Installing Dependencies']]],
   ['dependency_7',['Using as a Dependency',['../index.html#using-as-a-dependency',1,'']]],
   ['design_20philosophy_8',['Design Philosophy',['../index.html#design-philosophy',1,'']]],
